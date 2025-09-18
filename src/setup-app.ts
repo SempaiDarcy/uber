91eaec240d0c030e14ea064897c036bf3f69ad96
SempaiDarcy <mr_darcy_1@mail.ru>
2025-09-19 02:28:56 +0500

    feat(drivers): добавить валидацию тела запроса и e2e тесты
    - обновлён src/setup-app.ts:
      • подключена vehicleInputDtoValidation для POST /drivers
      • при ошибках возвращается 400 BadRequest с createErrorMessages
      • типизация ответов расширена (Driver | { errorsMessages: ValidationError[] })
    
    - добавлен __tests__/e2e/drivers/drivers-body-validation.e2e.spec.ts:
      • проверка что при некорректном теле запроса POST /drivers возвращает 400
      • проверка разных сценариев валидации (пустые строки, неверные email/number, короткие значения и т.д.)
      • убедились что при ошибках водитель не создаётся

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,11 +2,13 @@ import express, {Request, Response, Express} from "express";
 import {db} from "./db/in-memory-db";
 import {Driver} from "./drivers/types/driver";
 import {HttpStatus} from "./core/types/http-statuses";
+import {createErrorMessages} from "./core/utils/error.utils";
+import {ValidationError} from "./drivers/types/validationError";
+import {vehicleInputDtoValidation} from "./drivers/validation/vehicleInputDtoValidation";
 
 export const setupApp = (app: Express) => {
-    app.use(express.json()); // middleware для парсинга JSON в теле запроса
+    app.use(express.json());
 
-    // основной роут
     app.get("/", (req: Request, res: Response) => {
         res.status(HttpStatus.Ok).send("Hello world!");
     });
@@ -19,11 +21,20 @@ export const setupApp = (app: Express) => {
 
     app.get('/drivers/:id', (
         req: Request<{ id: string }, Driver, {}, {}>,
-        res: Response<Driver | null>
+        res: Response<Driver | {
+            errorsMessages: ValidationError[]
+        }>
     ) => {
         const driver = db.drivers.find(el => el.id === +req.params.id)
         if (!driver) {
-            res.status(404);
+            res
+                .status(HttpStatus.NotFound)
+                .send(
+                    createErrorMessages([{
+                        field: 'id',
+                        message: 'Driver not found'
+                    }]),
+                );
             return; // Если не поставить return, то после отправки 404 Express попробует ещё раз отправить 200 → и ты получишь ошибку
         }
         res.status(HttpStatus.Ok).send(driver);
@@ -31,7 +42,9 @@ export const setupApp = (app: Express) => {
 
     app.post('/drivers', (
         req: Request<{}, Driver, Omit<Driver, "id" | "createdAt">>,
-        res: Response<Driver>) => {
+        res: Response<Driver | {
+            errorsMessages: ValidationError[]
+        }>) => {
         const {
             name,
             phoneNumber,
@@ -44,7 +57,10 @@ export const setupApp = (app: Express) => {
             vehicleFeatures
         } = req.body;
         //1) проверяем приходящие данные на валидность
-
+        const errors = vehicleInputDtoValidation(req.body)
+        if (errors.length>0) {
+            res.status(HttpStatus.BadRequest).send(createErrorMessages(errors))
+        }
         //2) создаем newDriver
         const newDriver: Driver = {
             id: db.drivers.length ? db.drivers[db.drivers.length - 1].id + 1 : 1,
